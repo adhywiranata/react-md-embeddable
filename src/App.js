6efb0679897989f02c1ef22a84449de29453e69f
Adhy <adhywiranatap@gmail.com>
2018-03-11 21:50:16 +0700

    major updates

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import * as fs from 'fs-web';
 import React, { Component } from 'react';
 import ReactMarkdown from 'react-markdown';
+import axios from 'axios';
+import './App.css';
+
+const lightTheme = {
+  color: 'black',
+};
+
+const darkTheme = {
+  background: '#222222',
+  color: '#FFFFFF',
+};
 
 class App extends Component {
   state = {
@@ -9,11 +20,16 @@ class App extends Component {
 
   componentDidMount() {
     this.setState({ source: 'loading...' });
+    axios('https://raw.githubusercontent.com/adhywiranata/react-md-embeddable/master/markdowns/001.md')
+      .then((res) => this.setState({ source: res.data }))
+      .catch(err => console.log(err))
   }
 
   render() {
     return (
-      <ReactMarkdown source={this.state.source} />
+      <div style={{...lightTheme, padding: 20 }}>
+        <ReactMarkdown source={this.state.source} />
+      </div>
     );
   }
 }
